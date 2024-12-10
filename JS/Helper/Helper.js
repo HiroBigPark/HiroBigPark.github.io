5b7455e6bba05ba01756dfe444760a2f22cd6a8c
HiroBigPark <126617284+HiroBigPark@users.noreply.github.com>
2024-12-10 12:15:35 +0900

    Update Helper.js

--- JS/Helper/Helper.js
+++ JS/Helper/Helper.js
@@ -1,18 +1,16 @@
 "use strict"; // 厳格モード構文
 // クラス定義＆コンストラクタ
-function Test(){
+var Test function (){
   _msg : "hello Test"
 }
 Test.msg = "Test";
 
-function Helper(){ 
+var Helper function (){ 
   //レシーバ値NGはnewだけする。
   _msg : "hello Helper"
 }
 //  静的プロパティ
 Helper.msg = "hello";
-/*
-Helper.prototype.create = function(){
+Helper.create = function(){
   return new Helper();
 };
-*/
