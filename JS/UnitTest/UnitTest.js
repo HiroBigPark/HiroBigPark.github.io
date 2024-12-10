cf67680000a81b4473ef528b2417fe1f4af76b29
HiroBigPark <126617284+HiroBigPark@users.noreply.github.com>
2024-12-11 01:03:06 +0900

    Update UnitTest.js

--- JS/UnitTest/UnitTest.js
+++ JS/UnitTest/UnitTest.js
@@ -1,7 +1,8 @@
-"use strict";
+"use strict"; // 厳格モード構文
 
 // クラス定義＆コンストラクタ
-var UnitTest = function (){
+var UnitTest=UnitTest||function(){};
+UnitTest = function (){
   _Title : "UnitTest",
   _Case : "Case",
   _CaseNo : 1
@@ -13,8 +14,6 @@ UnitTest.Case = "Case";
 UnitTest.CaseNo = 1;
 
 UnitTest.assert = function(p1){
-  if (!condition) {
-    console.assert(p1,UnitTest.Title+UnitTest.Case+UnitTest.CaseNo);
-  }
+  !p1||true?console.assert(p1,UnitTest.Title+UnitTest.Case+UnitTest.CaseNo);
   return p1;
 };
