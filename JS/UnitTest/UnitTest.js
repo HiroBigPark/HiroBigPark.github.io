5d035fac28536258de06d4f157715a358a1e7a1a
HiroBigPark <126617284+HiroBigPark@users.noreply.github.com>
2024-12-11 05:24:14 +0900

    Update UnitTest.js

--- JS/UnitTest/UnitTest.js
+++ JS/UnitTest/UnitTest.js
@@ -11,16 +11,12 @@ UnitTest = function (){
 */
 
 //  静的プロパティ
+
 UnitTest.Title = "UnitTest";
 UnitTest.Case = "Case";
 UnitTest.CaseNo = 0;
 
-UnitTest.start = function(p1,p2){
-  p1===undefined||UnitTest.Title=p1;
-  p2===undefined||UnitTest.Case=p1;
-  UnitTest.CaseNo = 0;
-  console.log("Start"+UnitTest.Title+" "+UnitTest.Case);
-}
+//  静的メソッド
 
 UnitTest.is = function(p1){
   UnitTest.CaseNo = UnitTest.CaseNo + 1;
@@ -31,4 +27,17 @@ UnitTest.is = function(p1){
 
 UnitTest.eq = function(p1,p2){
   return p1 === p2;
+}
+
+UnitTest.start = function(p1,p2){
+  p1===undefined||UnitTest.Title=p1;
+  p2===undefined||UnitTest.Case=p2;
+  UnitTest.CaseNo = 0;
+  console.log("Start"+UnitTest.Title+" "+UnitTest.Case);
+}
+
+UnitTest.case = function(p1,p2){
+  p1===undefined||UnitTest.Case=p1;
+  UnitTest.CaseNo = p2;
+  console.log("Start"+UnitTest.Title+" "+UnitTest.Case);
 }
